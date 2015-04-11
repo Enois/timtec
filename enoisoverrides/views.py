c657771efa641c929869aefa8496152280be8de4
Bruno Martin <bruno.martin@hacklab.com.br>
2015-04-11 17:20:29 -0300

    show all trampos in user profile, fix #112

--- enoisoverrides/views.py
+++ enoisoverrides/views.py
@@ -24,13 +24,13 @@ class EnoisProfileView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data()
-        context['portfolios'] = context['profile_user'].portfolio_set.all().order_by('-timestamp')[:4]
+        context['portfolios'] = context['profile_user'].portfolio_set.all().order_by('-timestamp')
         context['courses_given'] = [
             cp.course for cp in
-            context['profile_user'].courseprofessor_set.all()[:4]]
+            context['profile_user'].courseprofessor_set.all()]
         context['courses_enrolled'] = [
             cp.course for cp in
-            context['profile_user'].coursestudent_set.all()[:4]]
+            context['profile_user'].coursestudent_set.all()]
         return context
 
 
